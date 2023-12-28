f4378724ac93fecea2166b5c2abdc90f8217d760
anstxy-vi <anstxy.vi@outlook.com>
2023-12-28 17:16:04 +0800

    fix: test

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+--[[
+Author: anstxy-vi anstxy.vi@outlook.com
+Date: 2023-12-28 16:55:24
+LastEditors: anstxy-vi anstxy.vi@outlook.com
+LastEditTime: 2023-12-28 17:16:01
+FilePath: \nvim\init.lua
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+--]]
 require "core"
 
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
@@ -19,3 +27,5 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+--
